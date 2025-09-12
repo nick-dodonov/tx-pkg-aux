10584755690be03f6d02d86d1123dd5069399f1a
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-09-12 08:22:23 +0200

    simple lib stub with dep

---
+++ src/Log/Log.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace Log {
+    void DefaultInit();
+}
--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,15 @@
 add_rules("mode.debug", "mode.release")
 set_languages("c++17")
 
-target("tx-pkg-misc", function()
+add_requires("lwlog")
+
+target("tx-pkg-aux", function()
     set_kind("static")
 
+    add_files("src/Log/Log.cpp")
+    add_headerfiles("src/Log/Log.h", {prefixdir = "Log"})
+
     add_includedirs("src", {public = true})
+
+    add_packages("lwlog")
 end)
