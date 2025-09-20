f312fd180917a8abf9344f3443b079e99c5260af
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-09-20 20:49:26 +0200

    bzl test clion integration and debugging target (it works!)

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,10 +1,17 @@
 #include "Log/Log.h"
 
-int main() {
-    // Тестируем функциональность нашего логгера
+#include <format>
+
+int main(int argc, char* argv[])
+{
     Log::info("Test message from lwlog master branch");
     Log::warn("Warning test message");
     Log::error("Error test message");
-    
+
+    Log::info(std::format("Command line arguments count: {}", argc));
+    for (int i = 0; i < argc; ++i) {
+        Log::info(std::format("Argument {}: {}", i, argv[i]));
+    }
+
     return 0;
 }
