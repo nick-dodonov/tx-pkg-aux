d0f2f96508a71d7d772325a204ada326c664b6a8
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-11-05 08:51:37 +0300

    Add clang-format, clang-tidy configs and VSCode build task; reformat Log code

--- src/Log/Log.cpp
+++ src/Log/Log.cpp
@@ -1,14 +1,11 @@
 #include "Log.h"
 #include <lwlog.h>
 
-namespace Log
-{
+namespace Log {
     static std::shared_ptr<lwlog::basic_logger<lwlog::sinks::stdout_sink>> g_logger;
 
-    void DefaultInit()
-    {
-        g_logger = std::make_shared<
-            lwlog::basic_logger<lwlog::sinks::stdout_sink>>("default");
+    void DefaultInit() {
+        g_logger = std::make_shared<lwlog::basic_logger<lwlog::sinks::stdout_sink>>("default");
         // g_logger->set_level_filter(lwlog::level::info | lwlog::level::debug | lwlog::level::critical);
 
         // TODO: check/fix in web-tools and emrun
@@ -17,9 +14,9 @@ namespace Log
 
         // https://github.com/ChristianPanov/lwlog/tree/v1.4.0?tab=readme-ov-file#syntax
 #if defined(__EMSCRIPTEN__)
-#define SUBSECS "%e" // wasm precision isn't good
+    #define SUBSECS "%e" // wasm precision isn't good
 #else
-#define SUBSECS "%f" // microseconds is enough for native
+    #define SUBSECS "%f" // microseconds is enough for native
 #endif
         // TODO: {topic} supported
         // TODO: fix bug adding '.' in time crashes (so '·' is used instead)
@@ -27,39 +24,34 @@ namespace Log
         // TODO: {thread} depending on platform
         g_logger->set_pattern("[%T·" SUBSECS "] %t :<8%l [%n] %v");
 
-        //g_logger->debug("Logger initialized");
+        // g_logger->debug("Logger initialized");
     }
 
-    void Debug(const std::string &message)
-    {
+    void Debug(const std::string& message){
         if (!g_logger)
             DefaultInit();
         g_logger->debug(message.c_str());
     }
 
-    void Info(const std::string &message)
-    {
+    void Info(const std::string& message) {
         if (!g_logger)
             DefaultInit();
         g_logger->info(message.c_str());
     }
 
-    void Warn(const std::string &message)
-    {
+    void Warn(const std::string& message) {
         if (!g_logger)
             DefaultInit();
         g_logger->warning(message.c_str());
     }
 
-    void Error(const std::string &message)
-    {
+    void Error(const std::string& message) {
         if (!g_logger)
             DefaultInit();
         g_logger->error(message.c_str());
     }
 
-    void Fatal(const std::string &message)
-    {
+    void Fatal(const std::string& message) {
         if (!g_logger)
             DefaultInit();
         g_logger->critical(message.c_str());
