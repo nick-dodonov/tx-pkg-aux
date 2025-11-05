ed95702f2d60b84a1660fa984f3dc31744141549
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-11-05 09:03:47 +0300

    Add formatted logging methods and update .clang-format for template breaks

--- src/Log/Log.h
+++ src/Log/Log.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <format>
 #include <string>
 
 namespace Log {
@@ -9,4 +10,29 @@ namespace Log {
     void Warn(const std::string& message);
     void Error(const std::string& message);
     void Fatal(const std::string& message);
+
+    template <typename... Args>
+    void DebugF(std::format_string<Args...> fmt, Args&&... args) {
+        Debug(std::format(fmt, std::forward<Args>(args)...));
+    }
+
+    template <typename... Args>
+    void InfoF(std::format_string<Args...> fmt, Args&&... args) {
+        Info(std::format(fmt, std::forward<Args>(args)...));
+    }
+
+    template <typename... Args>
+    void WarnF(std::format_string<Args...> fmt, Args&&... args) {
+        Warn(std::format(fmt, std::forward<Args>(args)...));
+    }
+
+    template <typename... Args>
+    void ErrorF(std::format_string<Args...> fmt, Args&&... args) {
+        Error(std::format(fmt, std::forward<Args>(args)...));
+    }
+
+    template <typename... Args>
+    void FatalF(std::format_string<Args...> fmt, Args&&... args) {
+        Fatal(std::format(fmt, std::forward<Args>(args)...));
+    }
 }
