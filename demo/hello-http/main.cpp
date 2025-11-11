9547a11b75268fab81040c73713911a1fa183062
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-11-12 01:06:34 +0300

    Add async HTTP GET with DNS resolver and simplified main entry using coro

--- demo/hello-http/main.cpp
+++ demo/hello-http/main.cpp
@@ -1,36 +1,62 @@
 //#include "Http/SimpleClient.h"
 #include "Log/Log.h"
 #include <asio.hpp>
-#include <iostream>
-#include <thread>
+#include <functional>
 
 using namespace std::chrono_literals;
 
+static asio::awaitable<void> http_get_async(
+    const asio::any_io_executor& executor,
+    std::string_view url,
+    const std::function<void(std::string_view url)> handler)
+{
+    asio::ip::tcp::resolver resolver{executor};
+
+    // auto results = resolver.resolve("example.com", {});
+    // for (const auto& entry : results) {
+    //     auto endpoint = entry.endpoint();
+    //     Log::InfoF("Resolved endpoint: {}:{}", endpoint.address().to_string(), endpoint.port());
+    // }
+
+    // resolver.async_resolve("google.com", "asdf", [](const std::error_code& ec, const auto& results) {
+    //     Log::InfoF("Resolved results: count={} '{}'", results.size(), ec ? ec.message(): "<OK>");
+    //     for (const auto& entry : results) {
+    //         auto endpoint = entry.endpoint();
+    //         Log::InfoF("Resolved endpoint: {}:{}", endpoint.address().to_string(), endpoint.port());
+    //     }
+    // });
+
+    auto results = co_await resolver.async_resolve("google.com", "80", asio::use_awaitable);
+    for (const auto& entry : results) {
+        auto endpoint = entry.endpoint();
+        Log::InfoF("Resolved endpoint: {}:{}", endpoint.address().to_string(), endpoint.port());
+        handler(endpoint.address().to_string());
+    }
+}
+
+static void http_get(
+    const asio::any_io_executor& executor,
+    const std::string_view url,
+    std::function<void(std::string_view url)> handler)
+{
+    asio::co_spawn(executor, http_get_async(executor, url, std::move(handler)), asio::detached);
+}
+
 int main()
 {
-    std::ostringstream oss;
-    oss << std::this_thread::get_id();
-    Log::InfoF("[{}] Test SimpleClient stub", oss.view());
+    Log::Info("starting");
 
-    asio::steady_timer timer(asio::system_executor(), 5s);
-    timer.async_wait([](const std::error_code& ec)
-    {
-        Log::InfoF("Hello, Timer! {}", ec.message());
-    });
+    const asio::any_io_executor executor = asio::system_executor();
 
-    std::thread thread([](){
-        for (int i = 0; i <= 100; ++i) {
-            asio::post([i](){
-                std::ostringstream oss;
-                oss << std::this_thread::get_id();
-                std::cout << std::format("[{}] Thread: {}\n", oss.view(), i);
-            });
-        }
-    });
-    thread.join();
+    auto try_http_get = [&executor](std::string_view url) {
+        http_get(executor, url, [url](std::string_view result) { 
+            Log::InfoF("http_get result: {}: {}", url, result); 
+        });
+    };
+    try_http_get("http://ifconfig.io/");
+    try_http_get("https://httpbin.org/headers");
 
     asio::detail::global<asio::system_context>().join();
-
-    Log::InfoF("[{}] Exiting", oss.view());
+    Log::Info("exiting");
     return 0;
 }
