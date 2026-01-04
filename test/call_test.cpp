a51cf1c9c4c462d3422990f0fc596c66de877157
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2026-01-04 11:49:59 +0100

    Alternate between two implementations in benchmarks to prevent devirtualization

--- test/call_test.cpp
+++ test/call_test.cpp
@@ -1,6 +1,5 @@
 #include <benchmark/benchmark.h>
 #include <functional>
-#include <vector>
 
 // Интерфейс
 class ICaller {
@@ -9,46 +8,77 @@ public:
     virtual int call(int x) = 0;
 };
 
-// Реализация интерфейса
-class Caller : public ICaller {
+// Реализации интерфейса
+class Caller1 : public ICaller {
 public:
     int call(int x) override {
         return x + 1;
     }
 };
 
-// Функция для std::function
-int free_call(int x) {
+class Caller2 : public ICaller {
+public:
+    int call(int x) override {
+        return x + 2;
+    }
+};
+
+// Фабрика для предотвращения девиртуализации
+__attribute__((noinline)) ICaller* create_caller(int type) {
+    if (type == 1) {
+        return new Caller1();
+    }
+    return new Caller2();
+}
+
+// Функции для std::function
+int free_call1(int x) {
     return x + 1;
 }
 
+int free_call2(int x) {
+    return x + 2;
+}
+
 static void BM_VirtualInterfaceCall(benchmark::State& state) {
-    Caller caller_impl;
-    ICaller* interface_ptr = &caller_impl;
+    ICaller* caller1 = create_caller(1);
+    ICaller* caller2 = create_caller(2);
+    
     int sum = 0;
     int i = 0;
     for (auto _ : state) {
-        benchmark::DoNotOptimize(sum += interface_ptr->call(i++));
+        // Чередуем вызовы, чтобы предотвратить предсказание перехода и девиртуализацию
+        benchmark::DoNotOptimize(sum += (i % 2 == 0 ? caller1 : caller2)->call(i));
+        i++;
     }
+    
+    delete caller1;
+    delete caller2;
 }
 BENCHMARK(BM_VirtualInterfaceCall);
 
 static void BM_StdFunctionFreeCall(benchmark::State& state) {
-    std::function<int(int)> std_func = free_call;
+    std::function<int(int)> func1 = free_call1;
+    std::function<int(int)> func2 = free_call2;
+    
     int sum = 0;
     int i = 0;
     for (auto _ : state) {
-        benchmark::DoNotOptimize(sum += std_func(i++));
+        benchmark::DoNotOptimize(sum += (i % 2 == 0 ? func1 : func2)(i));
+        i++;
     }
 }
 BENCHMARK(BM_StdFunctionFreeCall);
 
 static void BM_StdFunctionLambdaCall(benchmark::State& state) {
-    std::function<int(int)> std_func = [](int x) { return x + 1; };
+    std::function<int(int)> func1 = [](int x) { return x + 1; };
+    std::function<int(int)> func2 = [](int x) { return x + 2; };
+    
     int sum = 0;
     int i = 0;
     for (auto _ : state) {
-        benchmark::DoNotOptimize(sum += std_func(i++));
+        benchmark::DoNotOptimize(sum += (i % 2 == 0 ? func1 : func2)(i));
+        i++;
     }
 }
 BENCHMARK(BM_StdFunctionLambdaCall);
