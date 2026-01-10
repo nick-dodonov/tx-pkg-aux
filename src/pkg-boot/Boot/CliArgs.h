2ebb54e9fb8e20cee675f5529485402743547d6e
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2026-01-10 15:22:49 +0100

    Add GetIntArg to CliArgs and simplify exit code parsing in main.cpp

--- src/pkg-boot/Boot/CliArgs.h
+++ src/pkg-boot/Boot/CliArgs.h
@@ -1,13 +1,14 @@
 #pragma once
-#include <algorithm>
+#include <expected>
 #include <string_view>
+#include <system_error>
 #include <vector>
 
 namespace Boot
 {
     using CliArgsView = std::vector<std::string_view>;
 
-    class CliArgs : public CliArgsView
+    class CliArgs: public CliArgsView
     {
     public:
         CliArgs(const int argc, char** argv) noexcept
@@ -17,9 +18,7 @@ namespace Boot
             : CliArgsView(argv, argv + argc)
         {}
 
-        [[nodiscard]] bool Contains(const std::string_view str) const noexcept
-        {
-            return std::ranges::any_of(*this, [&](const auto& arg) { return arg == str; });
-        }
+        [[nodiscard]] bool Contains(std::string_view str) const noexcept;
+        [[nodiscard]] std::expected<int, std::error_code> GetIntArg(const size_t index) const noexcept;
     };
 }
