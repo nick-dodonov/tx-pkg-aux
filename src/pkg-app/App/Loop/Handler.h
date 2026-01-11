82d6b40a8bbb01629a89401763c0a83efc095c8a
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2026-01-11 10:58:37 +0100

    Add CompositeHandler to forward calls to multiple handlers in App::Loop

--- src/pkg-app/App/Loop/Handler.h
+++ src/pkg-app/App/Loop/Handler.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "UpdateCtx.h"
+#include <boost/intrusive/list.hpp>
+#include <ranges>
 
 namespace App::Loop
 {
@@ -15,7 +17,17 @@ namespace App::Loop
         virtual void Update(const UpdateCtx& ctx) = 0;
     };
 
+    /// Concept for handler-like types
     template <typename T>
+    concept HandlerLike = 
+        std::derived_from<T, IHandler>
+        || 
+        requires(T t) {
+            { *t } -> std::derived_from<IHandler>;
+        };
+
+    /// Wrapper to adapt handler-like types to IHandler
+    template <HandlerLike T>
     struct WrapHandler: IHandler
     {
         WrapHandler(T inner)
@@ -38,9 +50,13 @@ namespace App::Loop
     };
 
     /// Typed handler for specific runner type
-    class Handler: public IHandler
+    class Handler
+        : public IHandler
+        , public boost::intrusive::list_base_hook<boost::intrusive::link_mode<boost::intrusive::auto_unlink>>
     {
     public:
+        using List = boost::intrusive::list<Handler, boost::intrusive::constant_time_size<false>>;
+
         [[nodiscard]] const IRunner* GetRunner() const { return _runner; }
         [[nodiscard]] IRunner* GetRunner() { return _runner; }
 
@@ -49,5 +65,63 @@ namespace App::Loop
         void SetRunner(IRunner* runner) { _runner = runner; }
 
         IRunner* _runner{};
+    };
+
+    /// Composite handler that forwards calls to multiple handlers
+    struct CompositeHandler: Handler
+    {
+        void Add(Handler& handler)
+        {
+            _handlers.push_back(handler);
+            if (_running) {
+                handler.Start();
+            }
+        }
+
+        void Remove(Handler& handler)
+        {
+            auto it = _handlers.iterator_to(handler);
+            if (it == _handlers.end()) {
+                return;
+            }
+            if (_running) {
+                handler.Stop();
+            }
+            _handlers.erase(it);
+        }
+
+        bool Start() override
+        {
+            for (auto it = _handlers.begin(); it != _handlers.end(); ++it) {
+                if (!it->Start()) {
+                    while (it != _handlers.begin()) {
+                        --it;
+                        it->Stop();
+                    }
+                    return false;
+                }
+            }
+            _running = true;
+            return true;
+        }
+
+        void Stop() override
+        {
+            _running = false;
+            for (auto& handler : std::ranges::reverse_view(_handlers)) {
+                handler.Stop();
+            }
+        }
+
+        void Update(const UpdateCtx& ctx) override
+        {
+            for (auto& handler : _handlers) {
+                handler.Update(ctx);
+            }
+        }
+        
+    private:
+        Handler::List _handlers;
+        bool _running{};
     };
 }
