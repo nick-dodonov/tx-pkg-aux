8c57454a147b5c36d4837870a63895ed220d495f
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2026-03-02 20:26:29 +0100

    Add rules_android dependency and clean up BUILD.bazel and cc_src.cpp

--- test/cc/cc_src.cpp
+++ test/cc/cc_src.cpp
@@ -1,22 +1,22 @@
 #include "Log/Log.h"
-#include "Boot/Boot.h"
+// #include "Boot/Boot.h"
 #include <gtest/gtest.h>
 
-static bool _fail = false;
+static bool fail = false;
 
 TEST(LogTest, Works) {
     Log::Info("Test info message");
     Log::Warn("Test warning message");
     Log::Error("Test error message");
-    EXPECT_TRUE(!_fail);
+    EXPECT_TRUE(!fail);
 }
 
 int main(int argc, char** argv)
 {
     if (argc > 1) {
-        _fail = true;
+        fail = true;
     }
-    Boot::DefaultInit(argc, const_cast<const char**>(argv));
+    // Boot::DefaultInit(argc, const_cast<const char**>(argv));
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
